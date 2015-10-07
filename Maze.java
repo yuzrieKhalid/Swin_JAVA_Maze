e352d6292996e42eda444e5320601b5be091a013
Yuzrie Khalid <yuyieyuzrie@gmail.com>
2015-10-07 19:51:49 +0800

    init git project

---
+++ Maze.java
@@ -0,0 +1,13 @@
+package yuzrie.javamaze;
+
+/**
+ * Created by yuzrie on 10/7/15.
+ * Class Type: Main Class
+ *
+ */
+public class Maze {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
